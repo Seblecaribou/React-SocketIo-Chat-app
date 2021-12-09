500eb8aaab377e81a8a9985b56845682a2b345b7
Seblecaribou <65242707+Seblecaribou@users.noreply.github.com>
2021-12-09 17:19:51 +0100

    Added context provider for contacts

--- src/components/NewConversationModal.js
+++ src/components/NewConversationModal.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Modal } from "react-bootstrap";
 
-export default function NewConversationModal() {
-  return <div>New Conversation</div>;
+export default function NewConversationModal({ closeModal }) {
+  return (
+    <>
+      <Modal.Header closeButton={closeModal}>Create Contact</Modal.Header>
+      <Modal.Body></Modal.Body>
+    </>
+  );
 }
