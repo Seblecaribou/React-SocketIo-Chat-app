a36ce3bc4145334471e9248fb198bfbc012f687f
Seblecaribou <65242707+Seblecaribou@users.noreply.github.com>
2021-12-13 14:29:31 +0100

    Added conv selectio, finished sidebar

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -1,5 +1,28 @@
 import React from "react";
+import { ListGroup } from "react-bootstrap";
+import { useConversations } from "../contexts/ConversationsProvider";
 
 export default function Conversation() {
-  return <div>Conversations</div>;
+  const { conversations, selectConversationIndex } = useConversations();
+
+  return (
+    <ListGroup variant="flush">
+      {conversations.map((conversation, index) => {
+        return (
+          <ListGroup.Item
+            key={index}
+            action
+            onClick={() => {
+              selectConversationIndex(index);
+            }}
+            active={conversation.selected}
+          >
+            {conversation.recipients
+              .map((recipient) => recipient.name)
+              .join(", ")}
+          </ListGroup.Item>
+        );
+      })}
+    </ListGroup>
+  );
 }
--- src/components/NewConversationModal.js
+++ src/components/NewConversationModal.js
@@ -1,11 +1,52 @@
-import React from "react";
-import { Modal } from "react-bootstrap";
+import React, { useState } from "react";
+import { useContacts } from "../contexts/ContactsProvider";
+import { Modal, Button, Form } from "react-bootstrap";
+import { useConversations } from "../contexts/ConversationsProvider";
 
 export default function NewConversationModal({ closeModal }) {
+  const [selectedContactIds, setSelectedContactIds] = useState([]);
+  const { contacts } = useContacts();
+  const { createConversation } = useConversations();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    createConversation(selectedContactIds);
+    closeModal();
+  }
+
+  function handleCheckboxChange(contactId) {
+    setSelectedContactIds((prevSelectedContactIds) => {
+      if (prevSelectedContactIds.includes(contactId)) {
+        return prevSelectedContactIds.filter((prevId) => {
+          return contactId !== prevId;
+        });
+      } else {
+        return [...prevSelectedContactIds, contactId];
+      }
+    });
+  }
+
   return (
     <>
       <Modal.Header closeButton={closeModal}>Create Contact</Modal.Header>
-      <Modal.Body></Modal.Body>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          {contacts.map((contact) => (
+            <Form.Group>
+              <Form.Check
+                type="checkbox"
+                value={selectedContactIds.includes(contact.id)}
+                label={contact.name}
+                onChange={() => handleCheckboxChange(contact.id)}
+              />
+            </Form.Group>
+          ))}
+          <Button type="submit" className="mt-3">
+            Send
+          </Button>
+        </Form>
+      </Modal.Body>
     </>
   );
 }
